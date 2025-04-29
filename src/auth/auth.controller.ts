a64858d7e33afb46fc7ac54ee3637a48cf0ebb57
KEWEN SILVA <kewensilva58@gmail.com>
2025-04-28 22:38:07 -0300

    :hammer: [Feat: add authetication]

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Prisma } from 'generated/prisma';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Inject()
+  private readonly authService: AuthService;
+
+  @Post()
+  signin(@Body() body: Prisma.UserCreateInput) {
+    return this.authService.signin(body);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Prisma, User } from 'generated/prisma';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  @Inject()
+  private readonly userService: UserService;
+
+  async signin(params: Prisma.UserCreateInput): Promise<Omit<User, "password">> {
+    const user = await this.userService.user({ email: params.email })
+    if(!user) throw new NotFoundException('Usuário não encontrado');
+
+    const passwordMatch = await bcrypt.compare(user.password, params.password);
+
+    if(!passwordMatch) throw new UnauthorizedException("Credenciais inválidas");
+
+    const { password, ...result } = user;
+    return result;
+  }
+}
