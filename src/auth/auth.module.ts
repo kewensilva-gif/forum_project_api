1e0d78451aa8e9ff1c94e051af9ea87e588fc90e
KEWEN SILVA <kewensilva58@gmail.com>
2025-04-29 12:12:10 -0300

    :hammer: [Feat: add token jwt module]

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,16 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
 import { PrismaService } from 'src/database/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY || '',
+      signOptions: { expiresIn: '80000s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserService, PrismaService]
 })
