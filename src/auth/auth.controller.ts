49292c83754d67e0142d07d7b2f2a9722f9270e1
KEWEN SILVA <kewensilva58@gmail.com>
2025-04-29 12:10:31 -0300

    :hammer: [Feat: add token jwt]

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Inject, Post } from '@nestjs/common';
 import { Prisma } from 'generated/prisma';
 import { AuthService } from './auth.service';
 
-@Controller('auth')
+@Controller('auth/login')
 export class AuthController {
   @Inject()
   private readonly authService: AuthService;
 
   @Post()
+  @HttpCode(HttpStatus.OK)
   signin(@Body() body: Prisma.UserCreateInput) {
     return this.authService.signin(body);
   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,26 @@
 import { Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
-import { Prisma, User } from 'generated/prisma';
+import { Prisma } from 'generated/prisma';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
-
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   @Inject()
   private readonly userService: UserService;
+  @Inject()
+  private readonly jwtService: JwtService
 
-  async signin(params: Prisma.UserCreateInput): Promise<Omit<User, "password">> {
+  async signin(params: Prisma.UserCreateInput): Promise<{ access_token: string }> {
     const user = await this.userService.user({ email: params.email })
-    if(!user) throw new NotFoundException('Usuário não encontrado');
 
-    const passwordMatch = await bcrypt.compare(user.password, params.password);
+    if(!user) throw new NotFoundException('Usuário não encontrado');
+    const passwordMatch = await bcrypt.compare(params.password, user.password);
 
     if(!passwordMatch) throw new UnauthorizedException("Credenciais inválidas");
 
-    const { password, ...result } = user;
-    return result;
+    const payload = { sub: user.id };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
