6aa9c1e41d6d92ff2708506d494f64ca081d5b15
KEWEN SILVA <kewensilva58@gmail.com>
2025-04-30 00:00:58 -0300

    :bug: [Fix: adjustment docker]

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Fórum')
+    .setDescription('The Forum API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('forum')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
